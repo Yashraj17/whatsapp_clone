ca9336b633bd1018e3539d115119d02f8a130918
Uttsav <uttsavkumar85410@gmail.com>
2022-06-11 18:55:01 +0530

    status change2

--- Src/Components/Status.js
+++ Src/Components/Status.js
@@ -4,10 +4,10 @@ import React from 'react'
 const Status = () => {
   return (
     <View>
-      <Text>Status my status is black 2 </Text>
-      <Text>Status my status is black 2 </Text>
-      <Text>Status my status is black 2 </Text>
-      <Text>Status my status is black 4 </Text>
+      <Text>Status 1 </Text>
+      <Text>Status  2</Text>
+      <Text>Status  3</Text>
+      <Text>Status 4</Text>
 
     </View>
   )
